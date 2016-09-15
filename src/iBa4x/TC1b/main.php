42d26b53641f1d3f57a5aa6277489655e00f5f3c
Khaled_Craft1b <iiBa4x@gmail.com>
2016-09-15 13:25:38 +0300

    Update main.php

--- src/iBa4x/TC1b/main.php
+++ src/iBa4x/TC1b/main.php
@@ -1,3 +1,17 @@
 <?php
 namespace iBa4x\TC1b;
+class main extends \pocketmine\plugin\PluginBase implements \pocketmine\event\Listener{
+  public function onEnable(){
+    $this->getServer()->getPluginManager()->registerEvents($this, $this);
+  }
+  public function onClick(\pocketmine\event\player\PlayerInteractEvent $event){
+    if($event->getItem()->getId() == 347){ $level = $this->getServer()->getLevelByName($event->getPlayer()->getLevel()->getName());
+      if($level->getTime() >= 12000){
+        $level->setTime(\pocketmine\level\Level::TIME_DAY);
+      }else{
+        $level->setTime(\pocketmine\level\Level::TIME_NIGHT);
+      }
+    }
+  }
+}
 ?>
